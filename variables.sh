3a900f93b6b3a5d7b7eb0330cf4b8383633a3190
SunYubo <yubogo@gmail.com>
2019-04-05 09:37:04 +0800

    first commit

---
+++ variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PRICE_PER_APPLE=5
+Low_case_name='Low case name'
+low_case_name='low case name'
+FILE_LIST=`ls`
+BIRTHDATE='Jan 1, 2000'
+BIRTHDAY=`date -d "$BIRTHDATE" +%A`
+
+echo 'PRICE_PER_APPLE is $PRICE_PER_APPLE'
+echo "PRICE_PER_APPLE is $PRICE_PER_APPLE"
+echo "Variables are case-sensitive, Low_case_name is $Low_case_name, low_case_name is $low_case_name"
+echo "File list is $FILE_LIST"
+echo "Birthdate is $BIRTHDATE, and birthday is $BIRTHDAY"
