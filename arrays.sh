2740f304f389da4655c6f616f111188d92a6096e
SunYubo <yubogo@gmail.com>
2019-04-05 11:32:48 +0800

    update

--- arrays.sh
+++ arrays.sh
@@ -3,4 +3,10 @@ my_array=(apple orange "Fruit basket" banana)
 
 echo "The total number is ${#my_array[@]}"
 echo "The first element is ${my_array[0]}"
+my_array[0]=pair
+echo "The first element is changed to ${my_array[0]}"
 echo "The last element is ${my_array[${#my_array[@]} - 1]}"
+
+my_array[${#my_array[@]} - 1]=pineapple
+echo "Newly added element is ${my_array[${#my_array[@]} - 1]}"
+
